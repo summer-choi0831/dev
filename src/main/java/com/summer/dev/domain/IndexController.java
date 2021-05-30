4b4f6ef9d390dea478ac278dd41cd758e561a096
bestselect <bestselect@linecorp.com>
2021-05-31 02:12:27 +0900

    로그 추가

--- src/main/java/com/summer/dev/domain/IndexController.java
+++ src/main/java/com/summer/dev/domain/IndexController.java
@@ -4,12 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class IndexController {
 
     @GetMapping("/wedding_card")
-    public String index(){
-        System.out.println("connect");
+    public String wedding_card(){
+        log.info("wedding_card 접속");
         return "wedding_card";
     }
 }
