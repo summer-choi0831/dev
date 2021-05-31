a14260d03795c07d8df8916cd7e03eb77cdac4c4
bestselect <bestselect@linecorp.com>
2021-05-31 22:53:29 +0900

    add img

--- src/main/frontend/app/src/components/WeddingCard.vue
+++ src/main/frontend/app/src/components/WeddingCard.vue
@@ -126,7 +126,7 @@ export default {
   data() {
     return {
       name: undefined,
-      mode: "end", // intro, start, question, end
+      mode: "intro", // intro, start, question, end
       question_num: 0,
       cur_answer: "",
       progress: 0,
